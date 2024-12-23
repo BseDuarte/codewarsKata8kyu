0340ae9bd365bad3c8ff89007ad1390859ec0868
BseDuarte <akerathromerin@gmail.com>
2024-12-23 10:21:05 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Kata
+{
+    public static string AreYouPlayingBanjo(string name)
+    {
+        //Implement me
+        if (!string.IsNullOrEmpty(name) && (name[0] == 'R' || name[0] == 'r'))
+        {
+            return $"{name} plays banjo";
+        }
+        else
+        {
+            return $"{name} does not play banjo";
+        }
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(AreYouPlayingBanjo("Martin"));
+        Console.WriteLine(AreYouPlayingBanjo("Rikke"));
+        Console.WriteLine(AreYouPlayingBanjo("bravo"));
+        Console.WriteLine(AreYouPlayingBanjo("rolf"));
+    }
+
+
+}
